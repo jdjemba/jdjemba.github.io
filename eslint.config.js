efd094ef8b421fa0e6bdfd0ae247ee192379b1dc
Junior Djemba <junior.djemba@eemi.com>
2024-11-13 12:46:22 +0100

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -17,9 +17,44 @@ export default tseslint.config(
 		],
 	},
 	js.configs.recommended,
-	...tseslint.configs.recommended,
-	...tseslint.configs.stylistic,
+	...tseslint.configs.recommendedTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
 	...eslintPluginAstro.configs.recommended,
 	...eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
 	perfectionist.configs['recommended-natural'],
+	{
+		languageOptions: {
+			parserOptions: {
+				project: './tsconfig.json',
+				// @ts-expect-error	ignore ts-check for this line
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			'@typescript-eslint/naming-convention': [
+				'error',
+				{
+					format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+					selector: 'variable',
+				},
+				{
+					format: ['PascalCase'],
+					selector: 'typeLike',
+				},
+				{
+					format: ['PascalCase'],
+					selector: 'class',
+				},
+				{
+					custom: {
+						match: false,
+						regex: '^I[A-Z]',
+					},
+					format: ['PascalCase'],
+					selector: 'interface',
+				},
+			],
+		},
+	},
 );
